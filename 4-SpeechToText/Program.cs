eb91e9d1cb7f1eb50febdf88975fc6730a9e9916
Jefercanya <jefercanya@hotmail.com>
2022-12-27 16:55:59 -0500

    Initial commit

---
+++ 4-SpeechToText/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+namespace _4_SpeechToText
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            var speechConfig = SpeechConfig.FromSubscription("YourSuscriptionKey", "YourServiceRegion");
+
+            Console.WriteLine("Speech to Text using Microphone");
+            await FromMic(speechConfig);
+
+            Console.WriteLine("\nSpeech to Text using File");
+            await FromFile(speechConfig);
+
+            Console.WriteLine("\nSpeech to Text using Stream");
+            await FromStream(speechConfig);
+
+            Console.ReadLine();
+        }
+
+        async static Task FromMic(SpeechConfig speechConfig)//Reconocimiento una sola vez
+        {
+            using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            using var recognizer = new SpeechRecognizer(speechConfig, "es-CO",audioConfig);//para especificar el idioma a escuchar por defecto es ingles
+            //using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+            Console.WriteLine("Speak into your microphone.");
+            var result = await recognizer.RecognizeOnceAsync();
+            Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+        }
+
+        async static Task FromFile(SpeechConfig speechConfig)//Reconocimiento una sola vez
+        {
+            using var audioConfig = AudioConfig.FromWavFileInput("test.wav");
+            //using var recognizer = new SpeechRecognizer(speechConfig, "es-CO", audioConfig);//para especificar el idioma a escuchar por defecto es ingles
+            using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+            var result = await recognizer.RecognizeOnceAsync();
+            Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+        }
+
+        async static Task FromStream(SpeechConfig speechConfig) //Reconocimiento continuo
+        {
+                var reader = new BinaryReader(File.OpenRead("test.wav"));
+                using var audioInputStream = AudioInputStream.CreatePushStream();
+                using var audioConfig = AudioConfig.FromStreamInput(audioInputStream);
+                using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+                byte[] readBytes;
+                do
+                {
+                    readBytes = reader.ReadBytes(1024);
+                    audioInputStream.Write(readBytes, readBytes.Length);
+                } while (readBytes.Length > 0);
+
+                var result = await recognizer.RecognizeOnceAsync();
+
+            switch (result.Reason)
+            {
+                case ResultReason.RecognizedSpeech:
+                    Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+                    break;
+                case ResultReason.NoMatch:
+                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                    break;
+                case ResultReason.Canceled:
+                    var cancellation = CancellationDetails.FromResult(result);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                    }
+                    break;
+            }
+         }        
+    }
+}
